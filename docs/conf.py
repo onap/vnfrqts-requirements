bcdcc217e0cded5521aa5bcf343f03a18def9298
Hagop Bozawglanian <hagop.bozawglanian@att.com>
2020-03-31 14:43:17 -0700

    VNFRQTS - Fixing requirement generation in new model
    Issue-ID: VNFRQTS-868
    
    Signed-off-by: Hagop Bozawglanian <hagop.bozawglanian@att.com>
    Change-Id: I610dfa9d6bdb42235feff7aea1a4631f13cfcfd9

--- docs/conf.py
+++ docs/conf.py
@@ -13,3 +13,24 @@ html_last_updated_fmt = '%d-%b-%y %H:%M'
 
 def setup(app):
     app.add_stylesheet("css/ribbon_onap.css")
+
+
+from docutils.parsers.rst import directives
+
+needs_extra_options = {
+    "target": directives.unchanged,
+    "keyword": directives.unchanged,
+    "introduced": directives.unchanged,
+    "updated": directives.unchanged,
+    "impacts": directives.unchanged,
+    "validation_mode": directives.unchanged,
+    "validated_by": directives.unchanged,
+    "test": directives.unchanged,
+    "test_case": directives.unchanged,
+    "test_file": directives.unchanged,
+    "notes": directives.unchanged,
+}
+
+needs_id_regex = "^[A-Z0-9]+-[A-Z0-9]+"
+needs_id_required = True
+needs_title_optional = True
